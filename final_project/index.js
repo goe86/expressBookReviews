1bb555eb24a72c3643b1d9f6ec7495b822db8375
goe86 <georgegamil1986@hotmail.com>
2024-10-24 03:38:26 -0400

    add authentication route function

--- final_project/index.js
+++ final_project/index.js
@@ -9,9 +9,38 @@ const app = express();
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+let users=[]
+
+// Check if the user with the given username and password exists
+const authenticatedCust = (username, password) => {
+    // Filter the users array for any user with the same username and password
+    let validCust = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validCust.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+  // Authenticate user
+    if (authenticatedCust(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("Customer successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+});
 });
  
 const PORT =5000;
