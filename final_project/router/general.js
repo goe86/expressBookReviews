62f2cc5c643953d45640c475d6d27ea099d5a6ae
goe86 <georgegamil1986@hotmail.com>
2024-10-25 15:53:17 -0400

    added more routes(public)

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,9 +23,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+let author=req.params.author
+let booksbyAuthor=[]
+Object.keys(books).forEach(key => {
+    let book = books[key]
+    if (book.author===author){
+        booksbyAuthor.push(book.title)
+    }})
+    if(booksbyAuthor.length>0){
+        return res.status(200).json(booksbyAuthor);
+    }else{
+        return res.status(404).json({ message: "No books found for the given author" });
+    }
+})
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
